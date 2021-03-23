3d6a06655bc1de3e68dbb263bb33590cac98938d
Kanaan <Kanaanwong@gmail.com>
2021-03-23 21:38:53 +0800

    c Lauguage

---
+++ hello.s
@@ -0,0 +1,28 @@
+	.file	"hello.c"
+	.text
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "hello, world\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	leaq	.LC0(%rip), %rcx
+	call	puts
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (GNU) 9.3.0"
+	.def	puts;	.scl	2;	.type	32;	.endef
